af7ea4496c3058d53b34207aa3b2b1b226dedca7
BugHunter-X <askzenai@gmail.com>
2025-01-04 23:00:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,32 @@
+```javascript
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />}>
+          <Route path="form" element={<ContactForm />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() { return <h1>Home</h1>; }
+function About() { return <h1>About</h1>; }
+function Contact() { 
+  const location = useLocation();
+  console.log(location.pathname);
+  return (
+    <div>
+      <h1>Contact</h1>
+      {location.pathname === '/contact/form' && <ContactForm/>}
+      </div>
+  );
+}
+function ContactForm() { return <h1>Contact Form</h1>; }
+export default App;
+```
